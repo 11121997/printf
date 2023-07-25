b87cbf29b17af6a83e96b5378d16b1ca81249a50
sabhilaila <lailasabhi7@gmail.com>
2023-07-25 05:37:10 -0700

    update

--- print_rot13.c
+++ print_rot13.c
@@ -3,8 +3,7 @@
 /**
  * print_rot13 - prints a string using rot13
  * @ap: list of arguments from _printf
- * @params: pointer to the struct flags that determines
- * if a falag is passed to _printf
+ * @params: pointer to the struct flags
  * Return: length of the printed string
  */
 int print_rot13(va_list ap, params_t *params)
@@ -17,16 +16,16 @@ int print_rot13(va_list ap, params_t *params)
 	(void)params;
 	for (j = 0; s[j]; j++)
 	{
-if (s[j] < 'A' || (s[j] > 'Z' && s[j] < 'a') || s[j] > 'z')
+	if (s[j] < 'A' || (s[j] > 'Z' && s[j] < 'a') || s[j] > 'z')
 	_putchar(s[j]);
-else
-{
+	else
+	{
 	for (i = 0; i <= 52; i++)
 	{
-if (s[j] == rot13[i])
+	if (s[j] == rot13[i])
 	_putchar(ROT13[i]);
 	}
-}
+	}
 	}
 
 	return (j);
