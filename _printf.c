7d296f8c22bc57eb075f948e7595767cb9a6caef
sabhilaila <lailasabhi7@gmail.com>
2023-07-25 12:55:08 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -8,7 +8,7 @@
 
 int _printf(const char *format, ...)
 {
-	int sum = 0;
+	int count = 0;
 	va_list ap;
 	char *p, *start;
 	params_t params = PARAMS_INIT;
@@ -24,7 +24,7 @@ int _printf(const char *format, ...)
 		init_params(&params, ap);
 		if (*p != '%')
 		{
-			sum += _putchar(*p);
+			count += _putchar(*p);
 			continue;
 		}
 		start = p;
@@ -38,12 +38,12 @@ int _printf(const char *format, ...)
 	if (get_modifier(p, &params))
 		p++;
 	if (!get_specifier(p))
-		sum += print_from_to(start, p,
+		count += print_from_to(start, p,
 			params.l_modifier || params.h_modifier ? p - 1 : 0);
 	else
-		sum += get_print_func(p, ap, &params);
+		count += get_print_func(p, ap, &params);
 	}
 	_putchar(BUF_FLUSH);
 	va_end(ap);
-	return (sum);
+	return (count);
 }
