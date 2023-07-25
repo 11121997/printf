e4c9681fb1402f9f1d7ec1a5e5f7269392022c83
sabhilaila <lailasabhi7@gmail.com>
2023-07-25 00:42:01 -0700

    test

---
+++ _putchar.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+*_putchar - writes the characters to stdout
+@c: the charcter to pint
+*return: on succcess 1 error -1 and ero is set appropriately
+*/
+int _putcher(char c)
+{
+	return (write(1, &c,  1));
+}
